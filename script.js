35aaf1875c6aae2e2c2fd9c37bf28ee48cd47489
Bartek <118764028+barteqcz@users.noreply.github.com>
2024-05-07 00:51:47 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+function submitter() {
+    // Get form inputs
+    var title = document.getElementById("input-text-title").value;
+    var content = document.getElementById("input-text-content").value;
+
+    // Clone template content
+    var templateContent = document.getElementById("articleTemplate").content.cloneNode(true);
+
+    // Fill in template with form data
+    templateContent.querySelector("#articleTitle").textContent = title;
+    templateContent.querySelector("#articleContent").textContent = content;
+
+    // Convert template content to string
+    var htmlString = new XMLSerializer().serializeToString(templateContent);
+
+    // Create Blob from HTML string
+    var blob = new Blob([htmlString], { type: 'text/html' });
+
+    // Create a temporary link element
+    var link = document.createElement('a');
+    link.download = 'clanek.html'; // Set the download filename
+    link.href = window.URL.createObjectURL(blob); // Set the link URL to the Blob object
+    link.click(); // Simulate a click on the link to trigger the download
+}
