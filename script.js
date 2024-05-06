bd0c82b08c8bdffcfba691b98066741a1cd99403
Bartek <118764028+barteqcz@users.noreply.github.com>
2024-05-07 01:08:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function submitter() {
     // Get form inputs
     var title = document.getElementById("input-text-title").value;
     var content = document.getElementById("input-text-content").value;
-    var image = document.getElementById("input-image").value;
 
     // Clone template content
     var templateContent = document.getElementById("articleTemplate").content.cloneNode(true);
